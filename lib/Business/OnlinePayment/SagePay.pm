9503051f296f541d46a9231c97b5668df0359dab
Simon Elliott <cpan@papercreatures.com>
2011-11-26 21:55:38 +0000

    Import of ELLIOTT/Business-OnlinePayment-SagePay-0.14 from CPAN.
    gitpan-cpan-distribution: Business-OnlinePayment-SagePay
    gitpan-cpan-version:      0.14
    gitpan-cpan-path:         ELLIOTT/Business-OnlinePayment-SagePay-0.14.tar.gz
    gitpan-cpan-author:       ELLIOTT
    gitpan-cpan-maturity:     released

--- lib/Business/OnlinePayment/SagePay.pm
+++ lib/Business/OnlinePayment/SagePay.pm
@@ -1,6 +1,6 @@
 package Business::OnlinePayment::SagePay;
 {
-  $Business::OnlinePayment::SagePay::VERSION = '0.13';
+  $Business::OnlinePayment::SagePay::VERSION = '0.14';
 }
 # vim: ts=2 sts=2 sw=2 :
   
@@ -664,7 +664,7 @@ Business::OnlinePayment::SagePay - SagePay backend for Business::OnlinePayment
 
 =head1 VERSION
 
-version 0.13
+version 0.14
 
 =head1 SYNOPSIS
 
